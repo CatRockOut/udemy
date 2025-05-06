1b28af7cf2c7c20974a4f66cbb19ab7698019c9f
Євгеній <catrockout@icloud.com>
2025-05-06 08:54:27 +0300

    temp

--- ultimate-react/11-worldwise/src/App.jsx
+++ ultimate-react/11-worldwise/src/App.jsx
@@ -1,20 +1,62 @@
+import {useEffect, useState} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 import Product from './pages/Product.jsx';
 import Pricing from './pages/Pricing.jsx';
 import Homepage from './pages/Homepage.jsx';
+import Login from './pages/Login.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 import AppLayout from './pages/AppLayout.jsx';
-import Login from './pages/Login.jsx';
+import CityList from './components/CityList.jsx';
+import CountryList from './components/CountryList.jsx';
+
+const BASE_URL = 'http://localhost:9000';
 
 function App() {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                
+                setCities(data);
+            } catch (error) {
+                alert('There was an error fetching Cities...');
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        
+        fetchCities();
+    }, []);
+    
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Homepage/>}/>
+                <Route index element={<Homepage/>}/>
                 <Route path="product" element={<Product/>}/>
                 <Route path="pricing" element={<Pricing/>}/>
                 <Route path="login" element={<Login/>}/>
-                <Route path="app" element={<AppLayout/>}/>
+                <Route path="app" element={<AppLayout/>}>
+                    <Route
+                        index
+                        element={<CityList cities={cities} isLoading={isLoading}/>}
+                    />
+                    <Route
+                        path="cities"
+                        element={<CityList cities={cities} isLoading={isLoading}/>}
+                    />
+                    <Route
+                        path="countries"
+                        element={<CountryList cities={cities} isLoading={isLoading}/>}
+                    />
+                    <Route path="form" element={<p>Form</p>}/>
+                </Route>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </BrowserRouter>
---
+++ ultimate-react/11-worldwise/src/components/CityList.jsx
@@ -0,0 +1,19 @@
+import Spinner from './Spinner.jsx';
+import CityItem from './CityItem.jsx';
+import Message from './Message.jsx';
+import styles from './CityList.module.css';
+
+function CityList({cities, isLoading}) {
+    if (isLoading) return <Spinner/>;
+    if (!cities.length) return <Message message="Add your first city."/>;
+    
+    return (
+        <ul className={styles.cityList}>
+            {cities.map((city) => (
+                <CityItem city={city} key={city.id}/>
+            ))}
+        </ul>
+    );
+}
+
+export default CityList;
